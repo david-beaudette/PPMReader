be8ef70d21f9b08ea31c83b55f6549c3973a1ebe
david-beaudette <beaudette.david@gmail.com>
2019-04-20 20:31:21 -0400

    Update ppm_read.ino

--- examples/ppm_read/ppm_read.ino
+++ examples/ppm_read/ppm_read.ino
@@ -6,13 +6,15 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("ready");
+  ppmReader.start();
 }
 
 void loop()
 {
 
-  int count;
-  while(ppmReader.get(count) != 0){  //print out the servo values
+  uint8_t count = 0;
+  while(ppmReader.isReceiving()){  
+    // Print out the servo values
     Serial.print(ppmReader.get(count));
     Serial.print("  ");
     count++;
